703afc2bef0722cda4917289f82ffe3f3f8a7ef5
omaranoud <omaranoud31@gmail.com>
2023-11-30 15:35:17 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//console.log(window.document);
+
+//recuperation de l'élément input par son id firstcolor
+const firstcolorElt = document.getElementById('firstcolor');
+//console.log(firstcolorElt);
+
+//recuperation de l'élément input par son id secondecolor
+const secondecolorElt = document.getElementById('secondecolor');
+//console.log(secondecolorElt);
+
+//recuperation de l'élément body
+const bodyElt= document.querySelector('body');
+//console.log(bodyElt);
+
+
+firstcolorElt.addEventListener('input',function() {
+    //console.log(firstcolorElt.value);
+    bodyElt.style="background: linear-gradient(to right,"+firstcolorElt.value+","+secondecolorElt.value+");";
+});
+secondecolorElt.addEventListener('input',function() {
+    //console.log(secondecolorElt.value);
+    //bodyElt.style="background: linear-gradient(to right,"+firstcolorElt.value+","+secondecolorElt.value+");";
+    bodyElt.style=`background: linear-gradient(to right,${firstcolorElt.value},${secondecolorElt.value});`
+});
